913e5ea8d1f595b37c2dccee61ebe47a02d8b2c3
Ayça Akdemir <mabeaaa@gmail.com>
2021-05-06 19:59:53 +0300

    6.05

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +10,21 @@ namespace TravelTripProject.Controllers
 {
     public class DefaultController : Controller
     {
+        BlogManager blogManager = new BlogManager(new EfBlogDal());
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            var value = blogManager.GetAll();
+            return View(value);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var value = blogManager.GetAll();
+            return PartialView(value);
         }
     }
 }
